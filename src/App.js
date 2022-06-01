53fdc96c14ea6f694a25bf2a0061fd6dc4d9bde5
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-01 17:59:20 -0500

    Create App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import logo from './logo.svg';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+import BookPage from './pages/BookPage';
+import CategoryPage from './pages/CategoryPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<BookPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+      </Routes>
+    </Router>
   );
 }
 
