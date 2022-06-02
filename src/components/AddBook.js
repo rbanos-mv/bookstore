b4882e06e37e5e505d418b397073b1c8d2c432a9
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-02 05:22:49 -0500

    Remove category

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,17 +5,7 @@ const AddBook = () => (
     <h3>Add new book</h3>
     <form>
       <input type="text" id="title" placeholder="Book Title" />
-      <select id="category" name="category">
-        <option value="" disabled>
-          Category1
-        </option>
-        <option value="" disabled>
-          Category2
-        </option>
-        <option value="" selected disabled>
-          Category3
-        </option>
-      </select>
+      <input type="text" id="author" placeholder="Author" />
       <button type="button">Add Book</button>
     </form>
   </div>
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,12 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = (props) => {
-  const {
-    id, title, author, category,
-  } = props;
+  const { id, title, author } = props;
   return (
     <li>
-      <h5>{category}</h5>
       <h4>{title}</h4>
       <h6>{author}</h6>
       <button type="button" id={id}>
@@ -22,6 +19,5 @@ export default Book;
 Book.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
