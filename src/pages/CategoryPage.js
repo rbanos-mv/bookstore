557abf520fd700e950dd72566f41515897a65560
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-07 05:20:44 -0500

    Add functionality for "Check status" button click

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const CategoryPage = () => <button type="button">Check status</button>;
+const CategoryPage = () => {
+  const dispatch = useDispatch();
+  const check = () => dispatch(checkStatus());
+  return (
+    <button type="button" onClick={check}>
+      Check status
+    </button>
+  );
+};
 
 export default CategoryPage;
