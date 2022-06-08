8aae382bc55e85e48ce97cac1a06d2965ad4c13d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-08 03:09:01 -0500

    Add middleware to configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
-import preloadedState from './initialData';
 
 const store = configureStore({
-  preloadedState,
   reducer: {
     books: booksReducer,
     categories: categoriesReducer,
   },
+  middleware: [thunk],
 });
 
 export default store;
