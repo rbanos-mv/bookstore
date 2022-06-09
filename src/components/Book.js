bd2ab3b64b79a9df169879d99ce58b1e4e44aa6c
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-09 05:49:23 -0500

    Add category, percent & chapter to book props

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,24 +4,51 @@ import PropTypes from 'prop-types';
 import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
-  const { id, title, author } = props;
+  const {
+    id, category, title, author, percent, chapter,
+  } = props;
   const dispatch = useDispatch();
   const remove = () => dispatch(removeBook(id));
   return (
     <li>
-      <h4>{title}</h4>
-      <h6>{author}</h6>
-      <button type="button" id={id} onClick={remove}>
-        Remove
-      </button>
+      <div className="bookInfo">
+        <h3 className="category">{category}</h3>
+        <h2 className="title">{title}</h2>
+        <h3 className="author">{author}</h3>
+        <button type="button" id={id} onClick={remove}>
+          Remove
+        </button>
+      </div>
+      <div className="bookProgress">
+        <div className="progressText">
+          <p className="presentage-text">
+            {percent}
+            %
+          </p>
+          <p className="completed-text">Completed</p>
+        </div>
+      </div>
+      <div className="bookChapter">
+        <p className="currentChapter">Current Chapter</p>
+        <p className="chapter">
+          Chapter&nbsp;
+          {chapter}
+        </p>
+        <button className="updateBtn" type="button">
+          Update progress
+        </button>
+      </div>
     </li>
   );
 };
 
 Book.propTypes = {
   id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  percent: PropTypes.number.isRequired,
+  chapter: PropTypes.number.isRequired,
 };
 
 export default Book;
