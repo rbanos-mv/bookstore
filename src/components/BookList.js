069184708df132e6fcb74f05eeb0bdef3b68ecb1
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-06-09 05:34:14 -0500

    Add category, percent & chapter to bookList props

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,23 +5,26 @@ import Book from './Book';
 const BookList = (props) => {
   const { books } = props;
   return (
-    <div>
-      <ul>
-        {books.map((book) => {
-          const { id, title, author } = book;
-          return (
-            <Book
-              key={id}
-              {...{
-                id,
-                title,
-                author,
-              }}
-            />
-          );
-        })}
-      </ul>
-    </div>
+    <ul>
+      {books.map((book) => {
+        const {
+          id, category, title, author, percent, chapter,
+        } = book;
+        return (
+          <Book
+            key={id}
+            {...{
+              id,
+              category,
+              title,
+              author,
+              percent,
+              chapter,
+            }}
+          />
+        );
+      })}
+    </ul>
   );
 };
 
@@ -29,8 +32,11 @@ BookList.propTypes = {
   books: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
+      percent: PropTypes.number.isRequired,
+      chapter: PropTypes.number.isRequired,
     }),
   ).isRequired,
 };
